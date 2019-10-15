b5781855f928e6568263453e0fc91ad462708e13
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-15 00:55:21 +0000

    updated eval and prep test data

---
+++ data_prep_test.py
@@ -0,0 +1,237 @@
+from datetime import datetime
+from functools import partial
+import glob
+from multiprocessing import Pool
+
+# Disable multiprocesing for numpy/opencv. We already multiprocess ourselves, this would mean every subprocess produces
+# even more threads which would lead to a lot of context switching, slowing things down a lot.
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+
+import sys
+import pandas as pd
+import cv2
+from PIL import Image
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+import scipy
+import scipy.ndimage
+import scipy.special
+from scipy.spatial.transform import Rotation as R
+
+from lyft_dataset_sdk.lyftdataset import LyftDataset
+from lyft_dataset_sdk.utils.data_classes import LidarPointCloud, Box, Quaternion
+from lyft_dataset_sdk.utils.geometry_utils import view_points, transform_matrix
+
+from config import cfg
+
+def create_transformation_matrix_to_voxel_space(shape, voxel_size, offset):
+    """
+    Constructs a transformation matrix given an output voxel shape such that (0,0,0) ends up in the center.
+    Voxel_size defines how large every voxel is in world coordinate, (1,1,1) would be the same as Minecraft voxels.
+    
+    An offset per axis in world coordinates (metric) can be provided, this is useful for Z (up-down) in lidar points.
+    """
+    
+    shape, voxel_size, offset = np.array(shape), np.array(voxel_size), np.array(offset)
+    
+    tm = np.eye(4, dtype=np.float32)
+    translation = shape/2 + offset/voxel_size
+    
+    tm = tm * np.array(np.hstack((1/voxel_size, [1])))
+    tm[:3, 3] = np.transpose(translation)
+    return tm
+
+def transform_points(points, transf_matrix):
+    """
+    Transform (3,N) or (4,N) points using transformation matrix.
+    """
+    if points.shape[0] not in [3,4]:
+        raise Exception("Points input should be (3,N) or (4,N) shape, received {}".format(points.shape))
+    return transf_matrix.dot(np.vstack((points[:3, :], np.ones(points.shape[1]))))[:3, :]
+
+def car_to_voxel_coords(points, shape, voxel_size, z_offset=0):
+    if len(shape) != 3:
+        raise Exception("Voxel volume shape should be 3 dimensions (x,y,z)")
+        
+    if len(points.shape) != 2 or points.shape[0] not in [3, 4]:
+        raise Exception("Input points should be (3,N) or (4,N) in shape, found {}".format(points.shape))
+
+    tm = create_transformation_matrix_to_voxel_space(shape, voxel_size, (0, 0, z_offset))
+    p = transform_points(points, tm)
+    return p
+
+def create_voxel_pointcloud(points, shape, voxel_size=(0.5,0.5,1), z_offset=0):
+
+    points_voxel_coords = car_to_voxel_coords(points.copy(), shape, voxel_size, z_offset)
+    points_voxel_coords = points_voxel_coords[:3].transpose(1,0)
+    points_voxel_coords = np.int0(points_voxel_coords)
+    
+    bev = np.zeros(shape, dtype=np.float32)
+    bev_shape = np.array(shape)
+
+    within_bounds = (np.all(points_voxel_coords >= 0, axis=1) * np.all(points_voxel_coords < bev_shape, axis=1))
+    
+    points_voxel_coords = points_voxel_coords[within_bounds]
+    coord, count = np.unique(points_voxel_coords, axis=0, return_counts=True)
+        
+    # Note X and Y are flipped:
+    bev[coord[:,1], coord[:,0], coord[:,2]] = count
+    
+    return bev
+
+def normalize_voxel_intensities(bev, max_intensity=16):
+    return (bev/max_intensity).clip(0,1)
+
+def move_boxes_to_car_space(boxes, ego_pose):
+    """
+    Move boxes from world space to car space.
+    Note: mutates input boxes.
+    """
+    translation = -np.array(ego_pose['translation'])
+    rotation = Quaternion(ego_pose['rotation']).inverse
+    
+    for box in boxes:
+        # Bring box to car space
+        box.translate(translation)
+        box.rotate(rotation)
+        
+def scale_boxes(boxes, factor):
+    """
+    Note: mutates input boxes
+    """
+    for box in boxes:
+        box.wlh = box.wlh * factor
+
+def draw_boxes(im, voxel_size, boxes, classes, z_offset=0.0):
+    for box in boxes:
+        # We only care about the bottom corners
+        corners = box.bottom_corners()
+        corners_voxel = car_to_voxel_coords(corners, im.shape, voxel_size, z_offset).transpose(1,0)
+        corners_voxel = corners_voxel[:,:2] # Drop z coord
+
+        class_color = classes.index(box.name) + 1
+        
+        if class_color == 0:
+            raise Exception("Unknown class: {}".format(box.name))
+
+        cv2.drawContours(im, np.int0([corners_voxel]), 0, (class_color, class_color, class_color), -1)
+
+def prepare_training_data_for_scene(first_sample_token, output_folder, bev_shape, voxel_size, 
+        z_offset,num_sweeps,min_distance,level5data):
+    """
+    Given a first sample token (in a scene), output rasterized input volumes and targets in birds-eye-view perspective.
+
+    """
+    sample_token = first_sample_token
+    
+    while sample_token:
+        
+        sample = level5data.get("sample", sample_token)
+
+        sample_lidar_token = sample["data"]["LIDAR_TOP"]
+        lidar_data = level5data.get("sample_data", sample_lidar_token)
+        lidar_filepath = level5data.get_sample_data_path(sample_lidar_token)
+
+        ego_pose = level5data.get("ego_pose", lidar_data["ego_pose_token"])
+        calibrated_sensor = level5data.get("calibrated_sensor", lidar_data["calibrated_sensor_token"])
+
+
+        global_from_car = transform_matrix(ego_pose['translation'],
+                                           Quaternion(ego_pose['rotation']), inverse=False)
+
+        car_from_sensor = transform_matrix(calibrated_sensor['translation'], Quaternion(calibrated_sensor['rotation']),
+                                            inverse=False)
+
+        try:
+            lidar_pointcloud = LidarPointCloud.from_file_multisweep(level5data,sample,'LIDAR_TOP','LIDAR_TOP',num_sweeps=num_sweeps,min_distance=min_distance)[0]
+            lidar_pointcloud.transform(car_from_sensor)
+        except Exception as e:
+            print ("Failed to load Lidar Pointcloud for {}: {}:".format(sample_token, e))
+            sample_token = sample["next"]
+            continue
+        
+        bev = create_voxel_pointcloud(lidar_pointcloud.points, bev_shape, voxel_size=voxel_size, z_offset=z_offset)
+        bev = normalize_voxel_intensities(bev)
+
+        
+        # boxes = level5data.get_boxes(sample_lidar_token)
+
+        # target = np.zeros_like(bev)
+
+        # move_boxes_to_car_space(boxes, ego_pose)
+        # scale_boxes(boxes, box_scale)
+        # draw_boxes(target, voxel_size, boxes=boxes, classes=classes, z_offset=z_offset)
+
+        bev_im = np.round(bev*255).astype(np.uint8)
+        # target_im = target[:,:,0] # take one channel only
+
+        cv2.imwrite(os.path.join(output_folder, "{}_input.png".format(sample_token)), bev_im)
+        # cv2.imwrite(os.path.join(output_folder, "{}_target.png".format(sample_token)), target_im)
+        
+        sample_token = sample["next"]
+
+
+if __name__ == '__main__':
+
+    data_path = cfg.DATA.DATA_PATH
+    json_path = cfg.DATA.TEST_JSON_PATH
+
+    l5d = LyftDataset(data_path= data_path,json_path = json_path,verbose = True)
+   
+    os.makedirs(cfg.DATA.ARTIFACTS_FOLDER,exist_ok=True)
+
+    records = [(l5d.get('sample', record['first_sample_token'])['timestamp'], record) for record in l5d.scene]
+
+    entries = []
+
+    for start_time, record in sorted(records):
+        start_time = l5d.get('sample', record['first_sample_token'])['timestamp'] / 1000000
+
+        token = record['token']
+        name = record['name']
+        date = datetime.utcfromtimestamp(start_time)
+        host = "-".join(record['name'].split("-")[:2])
+        first_sample_token = record["first_sample_token"]
+
+        entries.append((host, name, date, token, first_sample_token))
+                
+    test_df = pd.DataFrame(entries, columns=["host", "scene_name", "date", "scene_token", "first_sample_token"])
+    # validation_hosts = ["host-a007", "host-a008", "host-a009"]
+
+    # validation_df = df[df["host"].isin(validation_hosts)]
+    #vi = validation_df.index
+    #train_df = df[~df.index.isin(vi)]
+
+    #train_data_folder = cfg.DATA.TRAIN_DATA_FOLDER
+    #validation_data_folder = cfg.DATA.VAL_DATA_FOLDER
+    num_workers = cfg.DATA.NUM_WORKERS
+    
+    test_data_folder = cfg.DATA.TEST_DATA_FOLDER
+
+
+    voxel_size = cfg.DATA.VOXEL_SIZE
+    z_offset = cfg.DATA.Z_OFFSET
+    bev_shape = cfg.DATA.BEV_SHAPE
+    # box_scale = cfg.DATA.BOX_SCALE
+    num_sweeps = cfg.DATA.NUM_SWEEPS
+    min_distance = cfg.DATA.MIN_DISTANCE
+    # classes = cfg.DATA.CLASSES
+
+    for df, data_folder in [(test_df, test_data_folder)]:
+        print("Preparing data into {} using {} workers".format(data_folder, num_workers))
+        first_samples = df.first_sample_token.values
+
+        os.makedirs(data_folder, exist_ok=True)
+    
+        process_func = partial(prepare_training_data_for_scene,
+                           output_folder=data_folder, bev_shape=bev_shape, voxel_size=voxel_size, 
+                           z_offset=z_offset,num_sweeps=num_sweeps,
+                           min_distance=min_distance,level5data=l5d)
+
+        pool = Pool(num_workers)
+        for _ in tqdm(pool.imap_unordered(process_func, first_samples), total=len(first_samples)):
+            pass
+        
+        pool.close()
+        del pool
